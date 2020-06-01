deb72d17626f39a2e163f909782144e7cf5a8b00
Heiko Reese <mail@heiko-reese.de>
2020-06-02 01:22:17 +0200

    Added MultiHash to calculate multiple hashes in one read

---
+++ multihash.go
@@ -0,0 +1,49 @@
+package shadowclonefs
+
+// this is heavily inspired by http://marcio.io/2015/07/calculating-multiple-file-hashes-in-a-single-pass/
+
+import (
+	"bufio"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"io"
+	"os"
+)
+
+type MultiHash struct {
+	MD5    string `json:"md5"`
+	SHA1   string `json:"sha1"`
+	SHA256 string `json:"sha256"`
+	SHA512 string `json:"sha512"`
+}
+
+func CalculateHashes(r io.Reader) (MultiHash, error) {
+	var (
+		err    error = nil
+		md5          = md5.New()
+		sha1         = sha1.New()
+		sha256       = sha256.New()
+		sha512       = sha512.New()
+	)
+
+	pagesize := os.Getpagesize()
+
+	// read input stream in chunks of the OS's page size
+	reader := bufio.NewReaderSize(r, pagesize)
+
+	// create Writer that copies to all hashes
+	multiWriter := io.MultiWriter(md5, sha1, sha256, sha512)
+
+	// copy the input to all hashes
+	_, err = io.Copy(multiWriter, reader)
+
+	return MultiHash{
+		hex.EncodeToString(md5.Sum(nil)),
+		hex.EncodeToString(sha1.Sum(nil)),
+		hex.EncodeToString(sha256.Sum(nil)),
+		hex.EncodeToString(sha512.Sum(nil)),
+	}, err
+}
